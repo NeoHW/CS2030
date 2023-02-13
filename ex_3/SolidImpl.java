e35b209942341c7f3262009033990dbbee10f222
NeoHW <neohaowei@gmail.com>
2023-02-14 01:33:16 +0800

    Completed exercise 3

---
+++ ex_3/SolidImpl.java
@@ -0,0 +1,23 @@
+public class SolidImpl implements Shape3D, Solid {
+    private final Shape3D shape;
+    private final double density;
+
+    SolidImpl(Shape3D shape, double density) {
+        this.shape = shape;
+        this.density = density;
+    }
+
+    public double volume() {
+        return this.shape.volume();
+    }
+    
+    public double mass() {
+        return this.shape.volume() * this.density; 
+    }
+
+    @Override
+    public String toString() {
+        return "SolidImpl";
+    }
+
+}
