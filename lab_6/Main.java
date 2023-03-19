5bc0c83539ad51a2dfcb2a46c766a7f2ac7227c7
NeoHW <neohaowei@gmail.com>
2023-03-19 21:40:38 +0800

    level 5 completed
    passed on codecrunch ,,
    but for some reason when running on own terminal , there is an extra D before the first output

--- lab_6/Main.java
+++ lab_6/Main.java
@@ -1,42 +1,34 @@
 import java.util.Scanner;
 
 class Main {
-    private static final int RECORD_NUM = 4;
         
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int numRecords = scanner.nextInt();
         scanner.nextLine(); //Important if your input ends with “\n”!
 
         Roster roster = new Roster("class");
         String output = "";
 
-        while (scanner.hasNext()) {
-            // reading in the records
-            for (int i = 0; i < numRecords; i++) {
-                for (int j = 0; j < RECORD_NUM; j++) {
-                    String stuID = scanner.next();
-                    String moduleCode = scanner.next();
-                    String assessTitle = scanner.next();
-                    String grade = scanner.next();
-                    roster.add(stuID, moduleCode, assessTitle, grade);
-                }
-            }
-    
-            // reading in the queries
-            while (true) {
-                try {
-                    String stuID = scanner.next();
-                    String moduleCode = scanner.next();
-                    String assessTitle = scanner.next();
-                    output = output + roster.getGrade(stuID, moduleCode, assessTitle) + "\n";
-                } catch (java.util.NoSuchElementException e) {
-                    break;
-                }
-            }
+        // reading in the records
+        for (int i = 0; i < numRecords; i++) {
+            String stuID = scanner.next();
+            String moduleCode = scanner.next();
+            String assessTitle = scanner.next();
+            String grade = scanner.next();
+            roster = roster.add(stuID, moduleCode, assessTitle, grade);
+            scanner.nextLine();
         }
-        
-        System.out.println(output);
+
+        while (scanner.hasNext()) {
+            String stuID = scanner.next();
+            String moduleCode = scanner.next();
+            String assessTitle = scanner.next();
+            output = output + roster.getGrade(stuID, moduleCode, assessTitle) + "\n";
+            scanner.nextLine();
+        }
+
+        scanner.close();
+        System.out.print(output);
     }
 }
