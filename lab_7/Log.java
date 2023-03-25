9d9e01189264c18b8c217fc8bf46f0209fda34de
NeoHW <neohaowei@gmail.com>
2023-03-25 17:42:37 +0800

    log.java now have 2 Optional<> attributes

--- lab_7/Log.java
+++ lab_7/Log.java
@@ -1,30 +1,28 @@
 import java.util.Optional;
 
 class Log<T> {
-    private final T value;
-    private final String log;
+    private final Optional<T> value;
+    private final Optional<String> log;
 
-    private Log(T value) {
-        this.value = value;
-        this.log = "";
-    }
-
-    private Log(T value, String log) {
+    private Log(Optional<T> value, Optional<String> log) {
         this.value = value;
         this.log = log;
     }
     
     static <T> Log<T> of(T t) {
-        return new Log<T>(t);
+        return Log.of(t, null);
     }
 
-    static <U> Log<U> of(U value, String log) {
-        return new Log<U>(value, log);
+    static <T> Log<T> of(T value, String log) {
+        return new Log<T>(Optional.<T>ofNullable(value), Optional.<String>ofNullable(log));
     }
 
     @Override
     public String toString() {
-        return String.format("Log[%s]\n%s", this.value, this.log);
+        if (log.isEmpty()) {
+            return String.format("Log[%s]", this.value.get());
+        } else {
+            return String.format("Log[%s]\n%s", this.value.get(), this.log.get());
+        }
     }
-
 }
