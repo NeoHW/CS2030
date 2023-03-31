d9e50d8e9b6fcb7991da04995152d62c7b0a0d4c
NeoHW <neohaowei@gmail.com>
2023-04-01 00:03:17 +0800

    finished mul

--- lab_8/Num.java
+++ lab_8/Num.java
@@ -38,17 +38,43 @@ class Num extends AbstractNum<Integer> {
         return new Num(this.opt.map(x -> s.apply(x)));
     }
 
+    // calling succ repeatedly
     Num add(Num other) {
         if (!(this.isValid() && other.isValid())) {
             return new Num(Optional.<Integer>empty());
         }
-        Num initial = Num.zero();
+        Num counter = Num.zero();
         Num ans = this;
-        while (!initial.equals(other)) {
+        while (!counter.equals(other)) {
             ans = ans.succ();
-            initial = initial.succ();
+            counter = counter.succ();
         }
         return ans;
     }
+
+    // m.mul(n) = m repeated additions of n
+    Num mul(Num other) {
+        if (!(this.isValid() && other.isValid())) {
+            return new Num(Optional.<Integer>empty());
+        }
+        if (this.equals(Num.zero()) || other.equals(Num.zero())) {
+            return Num.zero();
+        }
+        Num counter = Num.one();
+        Num ans = this;
+        Num initial = this;
+        while (!counter.equals(other)) {
+            ans = ans.add(initial);
+            counter = counter.succ();
+        }
+        return ans;
+    }
+
+    // m.sub(n) = (-n).add(m); negate n (without invalidating it)m then add m to it
+    /**
+    Num sub(Num other) {
+
+    }
+    */
 
 }
