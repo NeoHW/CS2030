1d04c11b197dd904594e98ac5434ba90d899e4cd
NeoHW <neohaowei@gmail.com>
2023-04-10 01:32:49 +0800

    can work for one input but not for more than one input in test.in:

--- ex_6/Main.java
+++ ex_6/Main.java
@@ -1,7 +1,9 @@
 import java.time.Instant;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.time.Duration;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
+import java.util.List;
 
 /**
  * This program finds different ways one can travel by bus (with a bit 
@@ -20,11 +22,23 @@ class Main {
     }
 
     public static void main(String[] args) {
+        /** 
         CompletableFuture<Void> cf1 = BusAPI.getBusStopsServedBy("95")
             .thenAccept(x -> System.out.println(x));
         CompletableFuture<Void> cf2 = BusAPI.getBusStopsServedBy("96")
             .thenAccept(x -> System.out.println(x));
         cf1.join();
         cf2.join();
+        */
+
+        Instant start = Instant.now();
+        Scanner sc = new Scanner(System.in);
+        List<CompletableFuture<BusRoutes>> result = sc.useDelimiter("\n").tokens()
+            .map(s -> processQuery(s))
+            .toList();
+        
+        result.stream()
+            .forEach(x -> System.out.println(x.join().description().join()));
     }
+    
 }
