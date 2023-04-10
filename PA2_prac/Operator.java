54d9d128c9896aeaac9b7faf6b9ab5177ae3db76
NeoHW <neohaowei@gmail.com>
2023-04-10 23:45:42 +0800

    public test cases for level 1 done

---
+++ PA2_prac/Operator.java
@@ -0,0 +1,39 @@
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+
+class Operator<T> implements Comparable<Operator<T>> {
+    private final int precValue;
+    private final BinaryOperator<T> operator;
+
+    private Operator(BinaryOperator<T> operator, int precValue) {
+        this.precValue = precValue;
+        this.operator = operator;
+    }
+
+    int getPrecValue() {
+        return this.precValue;
+    }
+
+    static <T> Operator<T> of(BinaryOperator<T> operator, int precValue) {
+        return new Operator<T>(operator, precValue);
+    }
+
+    T apply(T first, T second) {
+        return operator.apply(first, second);
+    }
+
+    @Override
+    public int compareTo(Operator<T> other) {
+        if (this.precValue == other.getPrecValue()) {
+            return 0;
+        } else {
+            return this.precValue > other.getPrecValue() ? 1 : -1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Operator of precedence " + this.precValue;
+    }
+
+}
