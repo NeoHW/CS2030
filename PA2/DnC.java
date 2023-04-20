199694a1ea3f9bb9564fb70addc11687871e18b9
NeoHW <neohaowei@gmail.com>
2023-04-20 17:23:42 +0800

    completed level 5 using walkthough

--- PA2/DnC.java
+++ PA2/DnC.java
@@ -6,19 +6,19 @@ import java.util.function.Supplier;
 import java.util.function.BinaryOperator;
 
 class DnC<T,R> {
-    private final T problem;
+    private final Supplier<T> problem;
     private final Predicate<T> pred; // returns true if atomic
     private final Function<T,R> soln; // only applies for atomic problems
-    private final Optional<Function<T,Pair<T,T>>> breakdown;
+    private final Optional<Function<T, Pair<Supplier<T>, Supplier<T>>>> breakdown;
 
-    protected DnC(T prob, Predicate<T> pred, Function<T,R> soln) {
+    protected DnC(Supplier<T> prob, Predicate<T> pred, Function<T,R> soln) {
         this.problem = prob;
         this.pred = pred;
         this.soln = soln;
         this.breakdown = Optional.empty();
     }
 
-    protected DnC(T prob, Predicate<T> pred, Function<T,R> soln, Optional<Function<T,Pair<T,T>>> breakdown) {
+    protected DnC(Supplier<T> prob, Predicate<T> pred, Function<T,R> soln, Optional<Function<T, Pair<Supplier<T>, Supplier<T>>>> breakdown) {
         this.problem = prob;
         this.pred = pred;
         this.soln = soln;
@@ -26,12 +26,16 @@ class DnC<T,R> {
     }
 
     static <T,R> DnC<T,R> of(T prob, Predicate<T> pred, Function<T,R> soln) {
-        return new DnC<T,R>(prob, pred, soln);
+        return new DnC<T,R>(() -> prob, pred, soln);
     }
 
     // level 3 overloaded of operator
     static <T,R> DnC<T,R> of(T prob, Predicate<T> pred, Function<T,R> soln, Function<T,Pair<T,T>> breakdown) {
-        return new DnC<T,R>(prob, pred, soln, Optional.of(breakdown));
+        return new DnC<T,R>(() -> prob, pred, soln, Optional.of(breakdown.andThen(pair -> Pair.of(() -> pair.first(), () -> pair.second()))));
+    }
+
+    static <T,R> DnC<T,R> of(Supplier<T> prob, Predicate<T> pred, Function<T,R> soln, Function<T,Pair<T,T>> breakdown, Function<T, Pair<Supplier<T>, Supplier<T>>> divider) {
+        return new DnC<T,R>(prob, pred, soln, Optional.of(divider));
     }
 
     // level 5 overloaded of operator
@@ -43,15 +47,23 @@ class DnC<T,R> {
 
     // this will be like toString()
     void peek(Consumer<T> action) {
-        action.accept(this.problem);
+        Optional.of(problem.get()).ifPresent(action);
     }
 
     Optional<R> solve() {
-        return Optional.of(problem).filter(pred).map(soln);
+        return this.solve(this.problem.get());
+    }
+
+    Optional<R> solve(T prob) {
+        return Optional.of(prob).filter(pred).map(soln);
     }
 
     DnC<T,R> left() {
-        return Optional.of(problem)
+        return this.left(problem.get());
+    }
+
+    DnC<T,R> left(T prob) {
+        return Optional.of(prob)
             .filter(Predicate.not(pred))
             .flatMap(x -> 
             breakdown.map(div -> {
@@ -61,9 +73,13 @@ class DnC<T,R> {
             })
         ).orElse(this);
     }
- 
+
     DnC<T,R> right() {
-        return Optional.of(problem)
+        return this.right(problem.get());
+    }
+
+    DnC<T,R> right(T prob) {
+        return Optional.of(prob)
             .filter(Predicate.not(pred))
             .flatMap(x -> 
             breakdown.map(div -> {
@@ -73,11 +89,15 @@ class DnC<T,R> {
     }
 
     // level 4
-    Optional<R> solve(BinaryOperator<R> op) {
-        return this.solve().or(
-            () -> left().solve(op)
-            .flatMap(lAns -> right().solve(op)
+    Optional<R> solve(T prob, BinaryOperator<R> op) {
+        return this.solve(prob).or(
+            () -> left(prob).solve(op)
+            .flatMap(lAns -> right(prob).solve(op)
             .map(rAns -> op.apply(lAns, rAns)))
         );
+    }
+
+    Optional<R> solve(BinaryOperator<R> op) {
+        return this.solve(this.problem.get(), op);
     }
 }
