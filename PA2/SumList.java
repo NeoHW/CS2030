b16982344d52666b7d43f8fcc0c874ad7816183d
NeoHW <neohaowei@gmail.com>
2023-04-20 17:50:57 +0800

    failed level 5 creating new dnc

--- PA2/SumList.java
+++ PA2/SumList.java
@@ -3,13 +3,16 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.List;
 
-class SumList extends DnC<Integer> {
-   sumList(List<Integer> list) {
-       super(list, );
-   }
+class SumList extends DnC<List<Integer>, Integer> {
+   
+    SumList(List<Integer> list) {
+       super(() -> list , x -> x.size() == 1,
+        x -> x.get(0), Optional.of(l -> {
+        int mid = (l.size() + 1) / 2;
 
-   SumList left() {
-
-   }
+        return Pair.of(() -> l.subList(0,mid), () -> l.subList(mid, l.size()));
+        }));
+    }
 }
